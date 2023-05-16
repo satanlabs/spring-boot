3ef9c5ff16fbeb27ce565bd0fb5688b4ed626988
admin <admin@satanlabs.com>
2023-05-16 17:17:20 +0530

    jar project changes
     - changed the test-controller's output type
     - changed the JAR file name
     - added logic for profile

--- jar_based/demo/src/main/java/com/example/demo/TestController.java
+++ jar_based/demo/src/main/java/com/example/demo/TestController.java
@@ -1,7 +1,10 @@
 package com.example.demo;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +13,21 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class TestController {
 
+	@Value("${spring.profiles.active}")
+	private String currentProfile;
+
+	@Value("${project.vendor.name}")
+	private String vendorName;
+
 	@GetMapping(value ="/test")
-	public String test() {
-		return (new Date() + " , hello");
+	public Map<String, Object> test() {
+		System.out.println("TestController.test() : incoming request : @ " + new Date());
+		Map<String, Object> map = new HashMap<>();
+
+		map.put("now", new Date().toString());
+		map.put("currentProfile", currentProfile);
+		map.put("VendorName", vendorName);
+
+		return map;
 	}
 }
